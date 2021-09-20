449366451be8d524746eb8c0a3f966ce7eb299ca
Josh Bowers <31971631+bowersj@users.noreply.github.com>
2021-09-19 22:43:54 -0600

    added client side search to episodes page

--- server/handlerHelpers.js
+++ server/handlerHelpers.js
@@ -2,5 +2,9 @@ const exphbs = require("express-handlebars");
 module.exports = {
     isGreaterThan1 (value) {
         return value > 1;
+    },
+
+    JSON2string( obj ) {
+        return JSON.stringify(obj);
     }
 };
